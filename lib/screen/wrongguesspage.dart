41e579fbc3350e43e7d22438bb7482d61ae9c838
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-02-15 22:58:11 +0530

    Created the Home Wrong Guess Page

--- lib/screen/wrongguesspage.dart
+++ lib/screen/wrongguesspage.dart
@@ -11,6 +11,7 @@ class _WrongGuessPageState extends State<WrongGuessPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 205, 210),
       appBar: AppBar(
         title: const Text(
           "Guess Game",
@@ -20,6 +21,44 @@ class _WrongGuessPageState extends State<WrongGuessPage> {
         ),
         backgroundColor: Colors.blue,
         elevation: 5,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Sorry! wrong guess. Please try again...'),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+              ),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Padding(
+                  padding: EdgeInsets.only(
+                    top: 10,
+                    bottom: 10,
+                  ),
+                  child: Text(
+                    'Try Again',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
